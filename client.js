8fc5573129f82bc9a241386247c8a61e7a5d1fd6
raberaucht <raberaucht@gmail.com>
2014-09-06 23:55:20 +0200

    add an event to the takeaways

--- client.js
+++ client.js
@@ -2,4 +2,10 @@ if (Meteor.isClient) {
   Template.takeaways.takeaways = function (){
     return Takeaways.find({});
   }  
+
+  Template.takeaways.events({
+    'click .template_name': function (event) {
+      console.log("worky");
+    }
+  })
 }
