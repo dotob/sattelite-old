2a3a784f7ee346d1fdbe4c145be5b3be0972673d
raberaucht <raberaucht@gmail.com>
2014-09-07 22:21:57 +0200

    create a foodrun on click on a takeaway

--- client.js
+++ client.js
@@ -1,15 +1,26 @@
 if (Meteor.isClient) {
-  Template.takeaways.takeaways = function () {
-    return Takeaways.find({});
-  }  
 
-  Template.takeaways.foodruns = function () {
-    return FoodRuns.find({});
-  }
-
-  Template.takeaways.events({
-    'click .template_name': function (event) {
-      FoodRuns.insert({});
+  Template.takeaways.helpers({
+    takeaways: function () {
+      return Takeaways.find({});
+    },
+    foodruns: function () {
+      return FoodRuns.find({});
     }
-  })
+  });
+
+  Template.takeaway.helpers({
+    log: function () {
+      console.log("Context here is:");
+      console.log(this);
+    }
+  });
+
+  Template.takeaway.events({
+    'click .template_name': function (event) {
+      var takeaway = Takeaways.findOne({_id: this._id});
+      FoodRuns.insert({takeaway: takeaway});
+    }
+  });
+
 }
