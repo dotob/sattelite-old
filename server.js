b69ac14088e99784bc6698b2875d02139f084699
raberaucht <raberaucht@gmail.com>
2014-09-07 13:18:49 +0200

    populate the takeaway collection

--- server.js
+++ server.js
@@ -1,10 +1,52 @@
 if (Meteor.isServer) {
   Meteor.startup(function () {
+
+    // if empty, populate the takeaway collection
     if(Takeaways.find().count() === 0) {
-      var takeaways = ["Toni",
-                       "Giovanni"]
-      for (var i = takeaways.length - 1; i >= 0; i--) {
-        Takeaways.insert({name: takeaways[i]});
+
+      // imports
+      var fs = Npm.require('fs'),
+        path = Npm.require('path');
+
+      // load fixtures
+      var basepath = path.resolve('.').split('.meteor')[0];
+      var fixtures = basepath + "/dishes.json";
+      var json = fs.readFileSync(fixtures, 'utf8');
+      var dishes = JSON.parse(json);
+
+      // index that stores if a takeaway is allready in the collection
+      // so the database doesn't get hit so hard
+      var takeaway_index = [];
+
+      // loop over the fixture and add dishes to the takeaways
+      for (var i=0; i < dishes.length; i++) {
+
+        // check if the takeaway is already on the index
+        if (!_.contains(takeaway_index, dishes[i].takeaway.name)) {
+
+          // if not, insert a new takeaway into the collection...
+          Takeaways.insert({
+            name:           dishes[i].takeaway.name,
+            description:    dishes[i].takeaway.description,
+            phone:          dishes[i].takeaway.phone,
+            dishes:         []
+          });
+
+          // ...and put the name of the takeaway on the index
+          takeaway_index.push(dishes[i].takeaway.name);
+        };
+
+        // store the dish's information in an object...
+        var dish = {
+          name:         dishes[i].name,
+          description:  dishes[i].description,
+          number:       dishes[i].order_number,
+          price:        dishes[i].price
+        };
+
+        // ...and add the dish-object to the takeaway's dishes
+        Takeaways.update({name: dishes[i].takeaway.name}, {$push: {dishes: dish}});
+
       };
     }
   })
