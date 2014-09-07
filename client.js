3fe9b95e0ce9a2a153e095bfb6948bbaa0f053ab
raberaucht <raberaucht@gmail.com>
2014-09-07 22:44:38 +0200

    remove log helper

--- client.js
+++ client.js
@@ -9,13 +9,6 @@ if (Meteor.isClient) {
     }
   });
 
-  Template.takeaway.helpers({
-    log: function () {
-      console.log("Context here is:");
-      console.log(this);
-    }
-  });
-
   Template.takeaway.events({
     'click .template_name': function (event) {
       var takeaway = Takeaways.findOne({_id: this._id});
