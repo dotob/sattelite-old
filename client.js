a1781607fd58f4457995442013ed8f40f9114f33
raberaucht <raberaucht@gmail.com>
2014-09-10 13:13:05 +0200

    add bespoken dishes to a foodrun

--- client.js
+++ client.js
@@ -3,8 +3,16 @@ if (Meteor.isClient) {
   Template.takeawaysListItem.events({
     'click .template_name': function (event) {
       var takeaway = Takeaways.findOne({_id: this._id});
-      FoodRuns.insert({takeaway: takeaway});
+      FoodRuns.insert({
+        takeaway: takeaway, 
+        bespoken: []
+      });
     }
   });
 
+  Template.foodrun.events({
+    'click .dishesName': function (event, foodrunTemplate) {
+      FoodRuns.update({_id: foodrunTemplate.data.foodrun._id}, {$push: {bespoken: this}});
+    }
+  });
 }
